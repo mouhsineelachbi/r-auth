3812f60cc65e19e57ddb77dc5693eae7a90970a4
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-10-03 15:40:43 +0100

    init getAuth global variable

--- utilities/firebase.js
+++ utilities/firebase.js
@@ -1,9 +1,7 @@
 // Import the functions you need from the SDKs you need
 
+import {getAuth} from 'firebase/auth'
 import { initializeApp } from "firebase/app";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,3 +15,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
