bcb8af75770f3edd959fe81e1ea88aa2f65a6574
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-10-03 17:14:18 +0100

    added user image when is logged in

--- components/nav.js
+++ components/nav.js
@@ -1,13 +1,28 @@
 import Link from "next/link";
+import {auth} from '../utils/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 export default function Nav() {
+
+  const [user, loading ] = useAuthState(auth);
+
   return (
     <nav className="flex items-center justify-between py-10">
       <Link href={"/doc"}>Logo</Link>
       <ul>
-        <Link href={'/auth/login'}>
-         <a className="py-2 px-4 bg-teal-700 text-white text-lg rounded-md ml-8 font-medium" href="">Join us</a>
-        </Link>
+        { !user &&
+          (<Link href={'/auth/login'}>
+          <a className="py-2 px-4 bg-teal-700 text-white text-lg rounded-md ml-8 font-medium" href="">Join us</a>
+         </Link>)
+        }
+
+        { user && (<div>
+          <h2 className="font-medium">{user.displayName}</h2>
+          <Link href={'/dashboard'}>
+            <img src={user.photoURL}  className="w-12 rounded-full"/>
+          </Link>
+          </div>
+        )}
       </ul>
     </nav>
   )
