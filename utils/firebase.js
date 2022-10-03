48ea2418d468c6d503c73fa0248699e591cb316e
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-10-03 22:42:19 +0100

    remove console.log

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,14 +2,13 @@ import {getAuth} from 'firebase/auth'
 import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
-console.log(process.env.API_KEY)
 const firebaseConfig = {
   apiKey: `${process.env.API_KEY}`,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  authDomain: `${process.env.AUTH_DOMAIN}`,
+  projectId: `${process.env.PROJECT_ID}`,
+  storageBucket: `${process.env.STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
+  appId: `${process.env.APP_ID}`
 };
 
 // Initialize Firebase
