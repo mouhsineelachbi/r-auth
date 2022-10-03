a2dfaeb72c12460aa06e8c28476f02ff253e7d16
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-10-03 16:03:14 +0100

    create join us button and align it in the center horizontally

--- components/nav.js
+++ components/nav.js
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default function Nav() {
   return (
-    <div>
-      <h1>nav</h1>
-    </div>
+    <nav className="flex items-center justify-between py-10">
+      <Link href={"/doc"}>Logo</Link>
+      <ul>
+        <Link href={'auth/login'}>
+         <a className="py-2 px-4 bg-teal-700 text-white text-lg rounded-md ml-8 font-medium" href="">Join us</a>
+        </Link>
+      </ul>
+    </nav>
   )
 }
